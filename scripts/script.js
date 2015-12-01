9f2d8a914a0a783acbcd2d166d4dbc91db49eb22
frostbyte100 <fulbel.leelabari@gmail.com>
2015-11-30 20:39:21 -0600

    added the script file and made some somes to science.html along with other files

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+localStorage.setItem("viewed", 1);
+
+$(document).ready(function(){
+
+    $("#check").click(function(){
+        $("#check").show();
+    });
+
+    $("#check").click(function(){
+        $("#check").hide();
+    });
+
+
+});
+
+
+localStorage.setItem("view") ++;
